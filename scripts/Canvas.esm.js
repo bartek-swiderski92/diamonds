f40f771fa2ad441af2f702cfd7462228b986f323
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-03 12:11:03 +0100

    add canvas

---
+++ scripts/Canvas.esm.js
@@ -0,0 +1,27 @@
+import { Common } from "./Common.esm.js";
+
+const GAME_SCREEN_ID = 'js-game-screen';
+const LOAD_CURRENT_ID = 'js-loading-screen-current';
+const LOAD_TOTAL_ID = 'js-loading-screen-total';
+
+export const CANVAS_WIDTH = 640;
+export const CANVAS_HEIGHT = 480;
+
+
+class Canvas extends Common {
+    constructor() {
+        super(GAME_SCREEN_ID);
+        this.configureCanvas();
+    }
+
+    configureCanvas() {
+        this.context = this.element.getContext('2d');
+        this.context.canvas.width = CANVAS_WIDTH;
+        this.context.canvas.height = CANVAS_HEIGHT;
+        this.context.font = '20px arial white';
+        this.context.fillStyle = 'white'
+    }
+
+}
+
+export const canvas = new Canvas();
--- scripts/LevelSelect.esm.js
+++ scripts/LevelSelect.esm.js
@@ -1,4 +1,6 @@
-import { Common, HIDDEN_SCREEN } from './Common.esm.js';
+import { Common, HIDDEN_SCREEN, VISIBLE_SCREEN } from './Common.esm.js';
+import { canvas } from './Canvas.esm.js';
+import { loader, DATALOADED_EVENT_NAME } from './Loader.esm.js'
 
 const gameLevels = [
     {
@@ -34,8 +36,13 @@ class LevelSelect extends Common {
 
     buttonOnClickHandler(event) {
         this.changeScreenVisibility(this.element, HIDDEN_SCREEN);
-        // TODO: show gameboard
+        this.changeScreenVisibility(canvas.element, VISIBLE_SCREEN);
+        this.loadLevel(event.currentTarget.value);
+    }
 
+    loadLevel(level) {
+        const background = loader.loadImage('./images/levelbackground.png')
+        window.addEventListener(DATALOADED_EVENT_NAME, () => console.log('Loaded '))
     }
 }
 
