5062c18231d16a0ffc1a87c3d83c86779575dc1a
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-03 14:21:49 +0100

    add initial gameState

--- scripts/Game.esm.js
+++ scripts/Game.esm.js
@@ -1,7 +1,13 @@
-import { Common } from "./Common.esm.js";
-import { gameLevels } from './GameLevels.esm.js'
-import { loader, DATALOADED_EVENT_NAME } from './Loader.esm.js'
+import { Common, VISIBLE_SCREEN } from "./Common.esm.js";
+import { gameLevels } from './GameLevels.esm.js';
+import { loader, DATALOADED_EVENT_NAME } from './Loader.esm.js';
+import { canvas } from "./Canvas.esm.js";
 
+const GameState = {
+    pointsToWin: 7000,
+    getPlayerPoints: () => 1000,
+    getMovesLeft: () => 30,
+}
 
 class Game extends Common {
     constructor() {
@@ -11,11 +17,12 @@ class Game extends Common {
     playLevel(level) {
         window.removeEventListener(DATALOADED_EVENT_NAME, this.playLevel);
         const levelInfo = gameLevels[level - 1];
+        this.changeScreenVisibility(canvas.element, VISIBLE_SCREEN)
         this.animate();
     }
 
     animate() {
-        console.log("Let's play")
+        canvas.drawGameOnCanvas(GameState);
         this.animationFrame = window.requestAnimationFrame(() => this.animate());
     }
 }
